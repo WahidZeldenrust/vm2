807e490d0b5a3674c4b6f9e21fcff7f46eed3853
WahidZeldenrust <43369237+WahidZeldenrust@users.noreply.github.com>
2020-05-20 15:16:06 +0200

    Database testing

--- Project-Vagrant-Ansible/cloudportal.sh
+++ Project-Vagrant-Ansible/cloudportal.sh
@@ -6,4 +6,4 @@ printf "########################################################################
 cd Klanten/Klant1/Test || exit
 
 vagrant up
-ansible-playbook /media/vagrant/vm2/Project Vagrant-Ansible/playbooks/playbook.yml
+ansible-playbook /media/vagrant/vm2/Project-Vagrant-Ansible/playbooks/playbook.yml
