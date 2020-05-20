942f7bbf3b7f9d1ae6553fe2cd9ee268e07fb689
WahidZeldenrust <43369237+WahidZeldenrust@users.noreply.github.com>
2020-05-20 21:28:27 +0200

    Database testing

--- Project-Vagrant-Ansible/cloudportal.sh
+++ Project-Vagrant-Ansible/cloudportal.sh
@@ -6,4 +6,10 @@ printf "########################################################################
 cd Klanten/Klant1/Test || exit
 
 vagrant up
+
+ssh-keyscan klant1-Test-webserver01 192.168.33.10 >> ~/.ssh/known_hosts
+ssh-keyscan klant1-Test-webserver02 192.168.33.11 >> ~/.ssh/known_hosts
+ssh-keyscan klant1-Test-loadbalancer 192.168.33.12 >> ~/.ssh/known_hosts
+ssh-keyscan klant1-Test-database 192.168.33.13 >> ~/.ssh/known_hosts
+
 ansible-playbook /media/vagrant/VM2/VM2/Project-Vagrant-Ansible/playbooks/playbook.yml
