8483fbf99df3cce4a2b4f0d0cc0a9497899056cd
WahidZeldenrust <43369237+WahidZeldenrust@users.noreply.github.com>
2020-05-20 14:34:22 +0200

    Database testing

--- Project Vagrant-Ansible/Klanten/roles/webservers/templates/index.php
+++ Project Vagrant-Ansible/Klanten/roles/webservers/templates/index.php
@@ -7,7 +7,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=VM2", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    echo "Connected successfully to the database";
 }
 catch(PDOException $e)
 {
@@ -22,7 +22,7 @@ catch(PDOException $e)
 	</head>
 
 	<body>
-		<a href="http://{{ ansible_default_ipv4.address }}/index.html">Homepage</a>
+		<a href="http://{{ ansible_default_ipv4.address }}/index.html"></a>
 		<p>Running on {{ inventory_hostname }}</p>
 
 		<form action="" method="post">
@@ -35,8 +35,10 @@ catch(PDOException $e)
         $name = (filter_input(INPUT_POST, name, FILTER_SANITIZE_STRING));
         $klantID = (filter_input(INPUT_POST, name, FILTER_SANITIZE_STRING));
 
-
-
+       $stmt = $conn->query("SELECT * FROM Klant");
+       while ($row = $stmt->fetch()) {
+           echo $row['KlantID']."<br />\n";
+       }
 		?>
 	</body>
 </html>
