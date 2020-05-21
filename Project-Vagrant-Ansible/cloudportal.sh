81a82707715e9aadc2264c8c65209059e87d207f
WahidZeldenrust <43369237+WahidZeldenrust@users.noreply.github.com>
2020-05-21 09:52:31 +0200

    Website and database fix

--- Project-Vagrant-Ansible/cloudportal.sh
+++ Project-Vagrant-Ansible/cloudportal.sh
@@ -6,4 +6,4 @@ printf "########################################################################
 cd Klanten/Klant1/Test || exit
 
 vagrant up
-ansible-playbook /media/vagrant/VM2/VM2/Project-Vagrant-Ansible/playbooks/playbook.yml
+ansible-playbook ~/Documents/vm2/Project-Vagrant-Ansible/playbooks/playbook.yml
