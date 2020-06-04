d866d19d8f3732c8a32e2b78d5e8f1639640fa69
WahidZeldenrust <43369237+WahidZeldenrust@users.noreply.github.com>
2020-06-04 13:08:09 +0200

    script editing

--- Project-Vagrant-Ansible/cloudportal.sh
+++ Project-Vagrant-Ansible/cloudportal.sh
@@ -3,9 +3,15 @@ printf "########################################################################
 printf "# Welcome to the cloudportal of VM2 \n"
 printf "###########################################################################\n"
 
+
 function newCustomer() {
     echo "Welcome to the cloudportal"
-    existingCustomer
+    cd /media/vagrant/vm2/vm/vm2/Project-Vagrant-Ansible/Klanten || exit
+
+    customerAmount="$(ls -1 | wc -l)"
+    newID=$( echo "$customerAmount + 1" | bc )
+
+    mkdir $"Klant$newID"
 
 }
 
@@ -17,7 +23,7 @@ function existingCustomer() {
     cd /media/vagrant/vm2/vm/vm2/Project-Vagrant-Ansible/Klanten/Klant$ID/Test || idError
 
     echo "What would you like to make?
-    (1) Test environment  (2) Production environment"
+(1) Test environment  (2) Production environment"
 
     read environment
 
@@ -39,7 +45,7 @@ function idError() {
 
 function start() {
     echo "Are you an existing customer?
-    (Y) I am (N) I am a new customer"
+(Y) I am (N) I am a new customer"
 
 read customer
 
