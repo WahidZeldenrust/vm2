b5d17c543180a231863423c56de451d09bd0a343
WahidZeldenrust <43369237+WahidZeldenrust@users.noreply.github.com>
2020-06-04 12:54:09 +0200

    script editing

--- Project-Vagrant-Ansible/cloudportal.sh
+++ Project-Vagrant-Ansible/cloudportal.sh
@@ -1,19 +1,59 @@
 printf "\n"
 printf "###########################################################################\n"
-printf "# Welcome to the cloudportaal of VM2 \n"
+printf "# Welcome to the cloudportal of VM2 \n"
 printf "###########################################################################\n"
 
 
-echo "Are you an existing customer?
-(Y) I am (N) I am a new customer"
+function start() {
+    echo "Are you an existing customer?
+        (Y) I am (N) I am a new customer"
 
 read customer
+
+
 if [ $customer == "Y" ] || [ $customer == "y" ]
 then
-    echo "You are a customer"
+    existingCustomer
 else
-    echo "You are not a customer"
+    newCustomer
 fi
+}
+
+start
+
+
+function newCustomer() {
+    echo "Welcome to the cloudportal"
+
+}
+
+function existingCustomer() {
+    echo "You are a customer"
+    echo "What is your customerID?"
+    read ID
+
+    cd /media/vagrant/vm2/vm/vm2/Project-Vagrant-Ansible/Klanten/Klant$ID || idError
+
+    echo "What would you like to make?
+    (1) Test environment  (2) Production environment"
+
+    read environment
+
+    if [ $environment == 1 ]
+    then
+      echo "test"
+    else
+
+      vagrant up
+      ansible-playbook /media/vagrant/vm2/vm/vm2/Project-Vagrant-Ansible/playbooks/playbook.yml
+
+    fi
+
+}
+
+function idError() {
+    echo "CustomerID does not exist"
+}
 
 #cd Klanten/Klant1/Test || exit
 #
