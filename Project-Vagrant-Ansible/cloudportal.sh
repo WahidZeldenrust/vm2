dc1f3366e8f0daaeeaf1083739ec158078338aa7
WahidZeldenrust <43369237+WahidZeldenrust@users.noreply.github.com>
2020-06-04 12:34:51 +0200

    script editing

--- Project-Vagrant-Ansible/cloudportal.sh
+++ Project-Vagrant-Ansible/cloudportal.sh
@@ -1,12 +1,24 @@
 printf "\n"
 printf "###########################################################################\n"
-printf "# Welkom bij de cloudportaal van VM2 \n"
+printf "# Welcome to the cloudportaal of VM2 \n"
 printf "###########################################################################\n"
 
-cd Klanten/Klant1/Test || exit
 
-vagrant up
+echo "Are you an existing customer?
+(Y) I am (N) I am a new customer"
 
+read customer
+if [ $customer == "Y" ]
+then
+    echo "You are a customer"
+else
+    echo "You are not a customer"
+fi
 
-
-ansible-playbook /media/vagrant/vm2/vm/vm2/Project-Vagrant-Ansible/playbooks/playbook.yml
+#cd Klanten/Klant1/Test || exit
+#
+#vagrant up
+#
+#
+#
+#ansible-playbook /media/vagrant/vm2/vm/vm2/Project-Vagrant-Ansible/playbooks/playbook.yml
